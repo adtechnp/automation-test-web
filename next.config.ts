3bf086ff9d396e3b4c017fcd0d8dc39e768a6381
dhirajraut1 <rautdhiraj277@gmail.com>
2025-05-21 22:02:01 +0545

    update yml

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['placehold.co']
-  }
+  },
+  output: 'export',
+  distDirectory: 'build',
+  trailingSlash: true,
+  basePath: '/automation-test-web',
 };
 
 export default nextConfig;
